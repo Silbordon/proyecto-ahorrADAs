cb05d61438c662675f1d99c006f7b4879d940e35
Silvina Bordon <silbordon.89@gmail.com>
2021-04-09 19:08:53 -0300

    CREATE/Se agrega layout para la seccion-balance

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Function Nav-Burger Responsive
+const burgerMenu = document.getElementById('navbar-burger')
+const navbarBasicExample = document.getElementById('navbarBasicExample')
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('is-active')
+    navbarBasicExample.classList.toggle('is-active')
+})
